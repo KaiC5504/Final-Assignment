0dab44fc2ed06a7aa61e29d6f0a793bf32021373
KaiC5504 <kaichuan2004@gmail.com>
2023-12-07 22:34:54 +0800

    Session Timeout

--- HTML/map.php
+++ HTML/map.php
@@ -1,3 +1,20 @@
+<?php
+    require('HTML/connect.php');
+    session_start();
+    if (isset($_SESSION['username'])) {
+        
+        if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 600)) {
+            session_unset();
+            session_destroy();
+            exit();
+        };
+        $_SESSION['LAST_ACTIVITY'] = time();
+    }
+    else {
+        session_unset();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
