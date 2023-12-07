be59faf15a2d0eee010353d6405d71451c4a23d8
KaiC5504 <kaichuan2004@gmail.com>
2023-12-07 23:27:22 +0800

    Profile Page

--- HTML/profile.php
+++ HTML/profile.php
@@ -7,9 +7,10 @@
         $row = mysqli_fetch_array($result);
         $username = $row['username'];
         $dob = $row['dob'];
-        echo "$username <br>"; 
-        echo "$dob";
-        if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 4)) {
+        $email = $row['email'];
+        // echo "$username <br>"; 
+        // echo "$dob";
+        if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1000)) {
             session_unset();
             session_destroy();
             header("Location: ../index.php");
@@ -32,11 +33,56 @@
     <link rel = "icon" href = "../resources/Images/Web_Icon.png">
     <script src="https://kit.fontawesome.com/64d3afc91e.js" crossorigin="anonymous"></script>
     <script src = "../JavaScript/hamburger.js" defer></script>
+
+    <style>
+        <?php include "../CSS/profile.css"; ?>
+    </style>
 </head>
 
 <header>
     <?php include "nav_bar.php"; ?>
+</header>
+
 <body>
-    
+    <div class = "profileContainer">
+        <div class = "leftBox">
+            <img src = "../resources/Images/Profile.png" alt = "Profile Picture">
+            <!-- <ul>
+                <li>Robert Downey Jr.</li>
+                <li>50 years</li>
+                <li>Actor</li>
+                <li>
+                    <i style="font-size:24px" class="fa"></i>
+                    <i style="font-size:24px" class="fa"></i>
+                    <i style="font-size:24px" class="fa"></i>
+                </li>
+            </ul> -->
+        </div>
+        <div class = "details">
+            <ul>
+                <h1>Details</h1>
+            </ul>
+            <ul>
+                <h3>Username</h3>
+                <?php echo "<li>$username</li>"; ?>
+            </ul>
+            <ul>
+                <h3>Birthday</h3>
+                <?php echo "<li>$dob</li>"; ?>
+            </ul>
+            <ul>
+                <h3>Account Created</h3>
+                <li>WILL BE ADDED IN</li>
+            </ul>
+            <!-- <ul>
+                <h3>More Info</h3>
+                <p>Ok</p>
+            </ul> -->
+            <ul>
+                <h3>Contact</h3>
+                <?php echo "$email"; ?>
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
